788b43e7e3c89e44a6354f4747462590dc08ccf9
Jim Mercurio <jamesm650@gmail.com>
2024-03-17 15:51:26 -0700

    Included example modal code commented out.

---
+++ assets/script.js
@@ -0,0 +1,18 @@
+// TYPICAL MODAL CODE
+
+// const modalBtn = document.querySelector(".modal-btn");
+// const modal = document.querySelector(".modal-overlay");
+// const closeBtn = document.querySelector(".close-btn");
+
+// modalBtn.addEventListener("click",function(){
+// modalBtn.classList.add("open-modal")
+
+// }
+
+// )
+// closeBtn.addEventListener("click",function(){
+//         modalBtn.classList.remove("open-modal");
+        
+// }
+
+// )
